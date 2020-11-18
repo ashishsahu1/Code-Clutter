5bc0b4baf4e8d29f99b3647699c6ba03cbd2d4b4
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-18 19:19:44 +0530

    LUCKFOUR

--- Biginner/sumOfDigit.py
+++ Biginner/sumOfDigit.py
@@ -1,11 +1,16 @@
 #https://www.codechef.com/problems/FLOW006
 
-n = int(input())
-for i in range(n):
-    sum = 0
-    x = int(input())
-    while(x>0):
-        sum = sum+x%10
-        x = x//10
+# n = int(input())
+# for i in range(n):
+#     sum = 0
+#     x = int(input())
+#     while(x>0):
+#         sum = sum+x%10
+#         x = x//10
     
-    print(sum)
+#     print(sum)
+
+lst = []
+for _ in range(int(input())):
+    lst = [int(x) for x in input()] 
+    print(sum(lst))
