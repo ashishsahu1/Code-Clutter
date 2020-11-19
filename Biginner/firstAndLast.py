aaec3d0b08a5a091c17e92bdb46342fd1e386d62
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-11-19 22:05:09 +0530

    tweak

--- Biginner/firstAndLast.py
+++ Biginner/firstAndLast.py
@@ -2,3 +2,13 @@ lst=[]
 for _ in range(int(input())):
     lst = [int(x) for x in input()]
     print(lst[0]+lst[len(lst)-1])
+
+# for i in range(int(input())):
+#     n = int(input())
+#     ld = n%10
+#     print(ld)
+#     while n>0:
+#         n = n//10
+#     fd = n
+#     print(fd)
+#     print(ld+fd)
