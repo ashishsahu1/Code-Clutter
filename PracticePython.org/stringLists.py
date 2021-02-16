7f0c06e60b3a3fcf2837c5afa6089e277cc39a4a
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-16 17:01:23 +0530

    palindrome

--- PracticePython.org/stringLists.py
+++ PracticePython.org/stringLists.py
@@ -1,17 +1,31 @@
-def check(lst):
-    for i in range(len(lst)):
-        if lst[i]!=lst[-(i+1)]:
-            return 1
-    return 0
+'''
+************Number palindrome check using loop***********************
+'''
+
+# def check(lst):
+#     for i in range(len(lst)):
+#         if lst[i]!=lst[-(i+1)]:
+#             return 1
+#     return 0
         
 
-lst = list(map(int,input().split()))
+# lst = list(map(int,input().split()))
 
-if(check(lst)):
-    print('Not Palindrome')
+# if(check(lst)):
+#     print('Not Palindrome')
+# else:
+#     print('palindrome')    
+
+'''
+************Word palindrome check***********************
+'''
+
+word = input("Enter the word : ")
+revWord = word[::-1]
+if word==revWord:
+    print('palindrome')
 else:
-    print('palindrome')    
-
+    print('not palindrome')
 
 
 
