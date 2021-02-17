909d471172829bf19ce772e0b0af68ac1bca4eb1
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-18 01:37:52 +0530

    prime number

--- PracticePython.org/PrimalityFunction.py
+++ PracticePython.org/PrimalityFunction.py
@@ -1,19 +1,38 @@
+import math
 
 def primeCheck(n):
-    if n==1 or n==0:
-        return False
-    elif n==2:
+    if n>1:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+            # else:
         return True
     else:
-        for i in range(2,(n/2)+1):
-            if n%i ==0:
-                return False
-                
+        return False
+
+def primeCheck2(n):
+    if n<=1:
+        return False
+    if n<=3:
+        return True
     
-            
+    for i in range(2,(math.ceil(math.sqrt(n)))+1):
+        if n%i==0:
+            return True
+    
+    return True
+
+      
 
 n = int(input('Enter the number : '))
-if primeCheck(primeCheck(int(n))):
+if primeCheck(int(n)):
     print('Prime')
 else:
     print('Not Prime')
+
+if primeCheck2(int(n)):
+    print('Prime')
+else:
+    print('Not Prime')
+
+
