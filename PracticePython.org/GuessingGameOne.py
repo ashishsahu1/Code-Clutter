e9c5c68ac72d86e6c2510884468784b69e395cce
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-17 15:54:49 +0530

    guess game

--- PracticePython.org/GuessingGameOne.py
+++ PracticePython.org/GuessingGameOne.py
@@ -1,11 +1,15 @@
 import random
 
-a = random.randint(1,9)
-x = int(input('Gues the number : '))
+while True:
+    a = random.randint(1,9)
+    x = input('Gues the number : ')
 
-if x==a:
-    print('Damn, you guessed it right')
-elif x<a:
-    print('Aaaahaa you are too low')
-else:
-    print('Aaaahaa you are too high')
+    if x=='exit':
+        break
+
+    if x==a:
+        print('Damn, you guessed it right')
+    elif x<a:
+        print('Aaaahaa you are too low')
+    else:
+        print('Aaaahaa you are too high')
